2be1301508e659ec0de04af05f2e0f81a68fdae0
dlrwtul <dibrahimalang@gmail.com>
2022-07-24 11:07:45 +0000

    integration page de connexion et d' inscription

--- src/app/authentification/authentification-routing.module.ts
+++ src/app/authentification/authentification-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthentificationComponent } from './authentification.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [{ path: '', component: AuthentificationComponent }];
+const routes: Routes = [
+  { 
+    path: '',
+    redirectTo: 'login' ,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login', 
+    component: LoginComponent 
+  },
+  {
+    path: 'signin', 
+    component: SigninComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/authentification/authentification.module.ts
+++ src/app/authentification/authentification.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AuthentificationRoutingModule } from './authentification-routing.module';
 import { AuthentificationComponent } from './authentification.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+
 
 
 @NgModule({
   declarations: [
-    AuthentificationComponent
+    AuthentificationComponent,
+    LayoutComponent,
+    LoginComponent,
+    SigninComponent,
   ],
   imports: [
     CommonModule,
